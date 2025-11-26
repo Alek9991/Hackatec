deeabcd339a876c7de2311cce7a2b22501958356
brau25-bit <brauliochaveza@gmail.com>
2025-11-25 23:33:54 -0600

    DB

---
+++ db.js
@@ -0,0 +1,20 @@
+// db.js
+import mysql from "mysql2/promise";
+
+export const db = await mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",           // tu contraseña
+  database: "lerma",      // tu base de datos
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Probar conexión:
+try {
+  const [rows] = await db.query("SELECT 1 + 1 AS resultado");
+  console.log("Conexión MySQL OK →", rows[0].resultado);
+} catch (err) {
+  console.error("Error conectando a MySQL:", err);
+}
